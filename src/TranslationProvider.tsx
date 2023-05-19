66671d05cc77a5118b523272454669e86fa3fb09
artem.tsvilii <artem.tsvilii@loveholidays.com>
2023-05-19 11:48:47 +0100

    fixing the lint

--- src/TranslationProvider.tsx
+++ src/TranslationProvider.tsx
@@ -24,7 +24,7 @@ export interface ReplaceArgumentErrorParams {
   key: string;
   argumentName: string;
   value: TranslationArgumentValue;
-};
+}
 
 interface TranslationProviderProps {
   locale: Locale;
